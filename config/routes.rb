84d100bba5b48c755389c1cce69a603710738f21
Wes Hargrove <whargrove@mac.com>
2013-10-01 20:35:28 -0700

    Update routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Vino::Application.routes.draw do
-  resources :posts
+	get 'signup', to: 'users#new', as: 'signup'
+	get 'login', to: 'sessions#new', as: 'login'
+	delete 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+	resources :posts
+	resources :sessions
   
   root 'posts#blog'
 end
