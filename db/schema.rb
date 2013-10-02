d79da72a9b7f5aa470f75adf3a154bfd7bb6dd27
Wes Hargrove <whargrove@mac.com>
2013-10-01 20:35:15 -0700

    Adds session and user model for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130820033907) do
+ActiveRecord::Schema.define(version: 20131002023931) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "user_name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
