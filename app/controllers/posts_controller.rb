c01c580d963ad0ba737517b68f86349f66da3bb2
Wes Hargrove <whargrove@mac.com>
2013-10-06 16:40:13 -0700

    Adds link and link_url to trusted params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -75,6 +75,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :user_id)
+      params.require(:post).permit(:title, :content, :user_id, :link, :link_url)
     end
 end
