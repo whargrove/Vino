c34e346aa637d01a41c5def51767eeeccbb99356
Wes Hargrove <whargrove@mac.com>
2013-10-06 12:02:12 -0700

    post#index orders posts same as post#blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.order('created_at DESC')
   end
 
   # GET /posts/1
