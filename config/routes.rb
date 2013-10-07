41ebbfe803fa56b253c62aa5a0b58931291bb9ab
Wes Hargrove <whargrove@mac.com>
2013-10-06 22:27:49 -0700

    Remove routes and controller actions for creating a new user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Vino::Application.routes.draw do
-	get 'signup', to: 'users#new', as: 'signup'
+	# get 'signup', to: 'users#new', as: 'signup'
 	get 'login', to: 'sessions#new', as: 'login'
 	delete 'logout', to: 'sessions#destroy', as: 'logout'
 
