3b6a17f93aa77715f05b9196c64d4b3c5b546fb4
Wes Hargrove <whargrove@mac.com>
2013-10-07 18:02:14 -0700

    Fixes dotenv file and resolves app error needing config.secret_key_base for app execution

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,6 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/.env #{release_path}/.env"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
