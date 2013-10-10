6a85331d4cd5e94bcf13e0c3ddac568c6d66631e
Wes Hargrove <whargrove@mac.com>
2013-10-09 20:44:10 -0700

    After successful login, user is redirected to /posts; Fixes #10.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_user_name(params[:user_name])
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
-  		redirect_to root_url, notice: "Logged in!"
+  		redirect_to posts_path
   	else
   		flash.now.alert = "Email or password is invalid"
   		render "new"
