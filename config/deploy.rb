1af197506ef7161a8c497c86c9e43b74d1718fdd
Wes Hargrove <whargrove@mac.com>
2013-11-09 18:38:03 -0800

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,35 +3,32 @@ set :repo_url, 'git@github.com:whargrove/Vino.git'
 set :brach, 'master'
 set :deploy_to, "/var/www/#{fetch(:application)}"
 set :scm, :git
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
 
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
+  before :deploy, 'deploy:check_revision'
+  
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      run "/etc/inid.d/unicorn_#{fetch(:application)} restart"
     end
   end
 
-  before :deploy, 'deploy:check_revision'
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'cache:clear'
+      end
     end
   end
 
