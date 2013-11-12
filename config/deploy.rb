9fd68753450a042d7730e20bd7251a9600db6822
Wes Hargrove <whargrove@mac.com>
2013-11-11 20:45:05 -0800

    Reduced capistrano log_level to :info

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :branch, 'master'
 set :deploy_to, "/var/www/#{fetch(:application)}"
 set :scm, :git
 set :format, :pretty
-set :log_level, :debug
+set :log_level, :info
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
