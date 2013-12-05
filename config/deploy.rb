4a5fa3469efb0f3d9faa45968ad33cdd2aeed879
Wes Hargrove <whargrove@mac.com>
2013-12-04 19:34:21 -0800

    Updated deploy.rb with rbenv_map_bins

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :ping_url, "http://weshargrove.com/ping"
 set :rbenv_type, :user
 set :rbenv_ruby, '2.0.0-p353'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
