582ca902d2cd189097e394894156d8569cbbd140
Wes Hargrove <whargrove@mac.com>
2013-12-04 19:30:03 -0800

    Update rbenv ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :ping_url, "http://weshargrove.com/ping"
 set :rbenv_type, :user
-set :rbenv_ruby, '2.0.0-p353'
+set :rbenv_ruby_version, '2.0.0-p353'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
