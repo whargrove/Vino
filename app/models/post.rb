7c69fb9ea9a076086d620d02b7f95ae3b6f0bec8
Wes Hargrove <whargrove@mac.com>
2014-01-03 00:46:07 -0800

    Closes #12 Adds link_url validations, updates Post model spec for validations.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,17 @@ class Post < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :user_id
   validates :link_url, presence: { if: :link }
+  validate :link_url_format_valid?
+
+  public
+
+  def link_url_format_valid?
+    uri = URI.parse(link_url)
+    if !uri.kind_of?(URI::HTTP)
+      errors.add(:link_url, "must be a valid format")
+    end
+  rescue URI::InvalidURIError
+    errors.add(:link_url, "must be a valid format")
+    false
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,7 +16,12 @@ describe Post do
 
   context 'link is true' do
     it 'is invalid without a link_url' do
-      expect(Post.new(link: true, link_url: nil)).to have(1).errors_on(:link_url)
+      expect(Post.new(link: true, link_url: nil)).to have(2).errors_on(:link_url)
+      # Expects two errors because both validates_presence_of :link_url and validates :link_url_format_valid? fail
+    end
+
+    it 'is invalid with a malformed URL' do
+      expect(Post.new(link: true, link_url: 'foo')).to have(1).errors_on(:link_url)
     end
   end
 end
