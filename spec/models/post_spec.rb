e74c41fd5f7f70b14e3211cfd94f59d39c9d8718
Wes Hargrove <whargrove@mac.com>
2014-01-02 22:04:03 -0800

    Updates post_spec.rb, refactor tests for Post model #18

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,30 @@
 require 'spec_helper'
 
 describe Post do
-  it "has no posts in the database" do
-    expect(Post).to have(:no).records
-    expect(Post).to have(0).records
+  it 'is valid with a title, content, user_id, link, and link_url' do
+    post = create(:post)
+    expect(post).to be_valid
   end
 
-  it "has one record" do
-    post = create(:post)
-    expect(Post.where(:title => 'post')).to have(1).record
+  it 'is invalid without a title' do
+    pending("Post model needs validations")
+    expect(Post.new(title: nil)).to have(1).errors_on(:title)
   end
 
-  it "counts only records that match a query" do
-    post = create(:post)
-    expect(Post.where(:title => 'post')).to have(1).record
-    expect(Post.where(:title => 'post_foo')).to have(0).record
+  it 'is invalid without content' do
+    pending("Post model needs validations")
+    expect(Post.new(content: nil)).to have(1).errors_on(:content)
+  end
+
+  it 'is invalid without a user_id' do
+    pending("Post model needs validations")
+    expect(Post.new(user_id: nil)).to have(1).errors_on(:user_id)
+  end
+
+  context 'link is true' do
+    it 'is invalid without a link_url' do
+      pending("Post model needs validations")
+      expect(Post.new(link_url: nil)).to have(1).errors_on(:link_url)
+    end
   end
 end
