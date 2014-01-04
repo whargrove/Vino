6e92b98b80b3246679b973c1de2f14e89b1e7af3
Wes Hargrove <whargrove@mac.com>
2014-01-04 14:18:02 -0800

    Implements StatHat call when GET #index is called.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order('created_at DESC').limit(5)
     Time.zone = 'Pacific Time (US & Canada)'
+
+    if Rails.env.production? && current_user.nil?
+      StatHat::API.ez_post_count("Blog Page View", CONFIG[:stathat_ez_api_key], 1)
+    end
   end
 
   # GET /posts/:id
