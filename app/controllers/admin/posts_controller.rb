21f9a3ac9ce65ae961ff6aeeda61a0115d2d96dc
Wes Hargrove <whargrove@mac.com>
2014-01-26 10:15:06 -0800

    [Finishes #64531572] Admin::PostsController and PostsController uses friendly find.

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -77,7 +77,8 @@ class Admin::PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      # Use friendly_id to find @post
+      @post = Post.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      # Use friendly_id to find @post
+      @post = Post.friendly.find(params[:id])
     end
 end
