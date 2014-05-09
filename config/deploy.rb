47afcd9c075292decb8ed91e9c74e696051132f0
Wes Hargrove <whargrove@mac.com>
2014-05-08 20:49:43 -0700

    Upgraded rbenv_version to 2.1.2 in config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :ping_url, "http://weshargrove.com/ping"
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.1'
+set :rbenv_ruby, '2.1.2'
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :keep_releases, 5
 
