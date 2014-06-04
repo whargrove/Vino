5520e90aaaa5c3ea4cf8fd3e7c2dfbedd36212c2
Wes Hargrove <whargrove@mac.com>
2014-06-04 07:19:43 -0700

    Added force_ssl

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   layout 'login'
+  force_ssl except: destroy, if: :ssl_configured?
 
   def new
     if current_user.nil?
@@ -10,21 +11,27 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by_user_name(params[:user_name])
-  	if user && user.authenticate(params[:password])
-  		session[:user_id] = user.id
-  		redirect_to admin_posts_path
-  	else
-  		redirect_to login_url, alert: "User name or password is invalid"
-  	end
+    user = User.find_by_user_name(params[:user_name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_posts_path
+    else
+      redirect_to login_url, alert: "User name or password is invalid"
+    end
   end
 
   def destroy
     if current_user
-    	session[:user_id] = nil
-    	redirect_to root_url, notice: "Logged out"
+      session[:user_id] = nil
+      redirect_to root_url, notice: "Logged out"
     else
       redirect_to login_url
     end
+  end
+
+private
+
+  def ssl_configured?
+    !Rails.env.development?
   end
 end
