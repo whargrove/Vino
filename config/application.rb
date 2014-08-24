6af0564c2f59fa2b0510591000abe8da9737f7ba
Wes Hargrove <whargrove@mac.com>
2014-08-24 12:13:49 -0700

    Added secrets and middleware config.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module Vino
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Set up Google Analytics
+    config.middleware.use Rack::GoogleAnalytics, :tracker => Rails.application.secrets.ga_tracker
   end
 end
