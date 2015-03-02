4ac8ea4bcfb1a8b738501b9cccf696267deb9ae2
Wes Hargrove <wesleyhargrove@gmail.com>
2015-03-01 17:22:05 -0800

    Updated deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,39 @@
+# This config is valid only for this version of Capistrano
+lock '3.3.5'
+
+# Application
 set :application, 'blog'
-set :repo_url, 'git@github.com:whargrove/Vino.git'
+set :repo_url, 'git@github.com:whargrove/vino.git'
+set :keep_releases, 5
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Only deploy from the master branch
 set :branch, 'master'
+
+# Default deploy_to is /var/www/app_name
 set :deploy_to, "/var/www/#{fetch(:application)}"
+
+# Git!
 set :scm, :git
+
+# Pretty stdout
 set :format, :pretty
+
+# Set to :debug if you want a more verbose output
 set :log_level, :info
-set :pty, true
-set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Rbenv
 set :rbenv_type, :user
 set :rbenv_ruby, '2.2.0'
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :keep_releases, 5
 
-# bundler
+# Bundler
 set :bundle_jobs, 4
 set :bundle_flags, '--deployment'
+
+# Other
+set :pty, true
 
 namespace :deploy do
 
@@ -36,6 +54,6 @@ namespace :deploy do
   after :rollback, 'deploy:restart'
 
   after :finishing, 'deploy:cleanup'
-  # after :finished, :create_release_file
+  after :finished, :create_release_file
 
 end
