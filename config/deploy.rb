435115c372faa4c14cc890b3d8d87dcb195a442c
Wes Hargrove <wesleyhargrove@gmail.com>
2015-03-01 16:55:45 -0800

    Add new stage, updated deploy script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,6 @@ set :log_level, :info
 set :pty, true
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :ping_url, "http://www.weshargrove.com/ping"
 set :rbenv_type, :user
 set :rbenv_ruby, '2.2.0'
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
@@ -20,21 +19,22 @@ set :bundle_flags, '--deployment'
 
 namespace :deploy do
 
-  before :deploy, 'check_revision'
+  before :deploy, :check_revision
 
-  desc 'Force restart of passenger after restart'
-  task :ping do
-    system "curl --silent #{fetch(:ping_url)}"
-  end
-
-  desc 'Create releast.txt file in public directory'
-  task :create_release_file do
-    on roles(:app) do
-      execute %{echo "#{revision_log_message}" > #{release_path.join('public/release.txt')}}
+  # Task to trigger Unicorn restart
+  %w[start stop restart].each do |command|
+    desc "#{command} Unicorn server"
+    task command do
+      on roles(:app) do
+        execute "/etc/init.d/unicorn_#{fetch(:application)} #{command} #{fetch(:rails_env)}"
+      end
     end
   end
 
-  after :finishing, 'deploy:ping'
+  # Restart Unicorn on deploy and rollback
+  after :deploy, 'deploy:restart'
+  after :rollback, 'deploy:restart'
+
   after :finishing, 'deploy:cleanup'
   after :finished, 'deploy:create_release_file'
 
